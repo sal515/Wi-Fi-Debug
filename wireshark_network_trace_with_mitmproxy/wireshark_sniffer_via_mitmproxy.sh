2e5375075a1676e65516da93e0624743d8435035
Salman Rahman <salman.rahman515@gmail.com>
2024-03-13 23:37:18 -0400

    wireshark sniffer setup script - added mitmproxy secure and insecure start and wireshark start cmds

--- wireshark_network_trace_with_mitmproxy/wireshark_sniffer_via_mitmproxy.sh
+++ wireshark_network_trace_with_mitmproxy/wireshark_sniffer_via_mitmproxy.sh
@@ -1,42 +1,102 @@
 #!/bin/bash
+# NOTE: Run this script with bash shell. It will not work with sh shell.
+# Example: bash wireshark_sniffer_via_mitmproxy.sh
 
-# If the first argument is not provided, use the default value
-SSLKEYLOGFILE=${1:-~/Desktop/ssl_key_log.log}
+# Logging functions
+# Log levels
+LOG_LEVEL_ERROR=0
+LOG_LEVEL_INFO=1
+LOG_LEVEL_DEBUG=2
 
+# Set the current log level
+CURRENT_LOG_LEVEL=$LOG_LEVEL_DEBUG
 
-# Parse command line arguments
-while (( "$#" )); do
+# Logging functions
+info() {
+  if [ $CURRENT_LOG_LEVEL -ge $LOG_LEVEL_INFO ]; then
+    local MESSAGE=$1
+    local TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+    echo "${TIMESTAMP} INFO ${MESSAGE}"
+  fi
+}
+
+debug() {
+  if [ $CURRENT_LOG_LEVEL -ge $LOG_LEVEL_DEBUG ]; then
+    local MESSAGE=$1
+    local TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+    echo "${TIMESTAMP} DEBUG ${MESSAGE}"
+  fi
+}
+
+error() {
+  if [ $CURRENT_LOG_LEVEL -ge $LOG_LEVEL_ERROR ]; then
+    local MESSAGE=$1
+    local TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+    echo "${TIMESTAMP} ERROR ${MESSAGE}" >&2
+  fi
+}
+
+# Default values
+if [ "$EUID" -eq 0 ]; then
+  USER_USERNAME=$SUDO_USER
+  USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
+else
+  USER_USERNAME=$USER
+  USER_HOME=$HOME
+fi
+info "Script executed as $USER_USERNAME and USER_HOME=$USER_HOME"
+
+SSLKEYLOGFILE=$USER_HOME/Desktop/ssl_key_log.log
+debug "SSL Key log file path set to $SSLKEYLOGFILE"
+
+while [ "$#" -gt 0 ]; do
   case "$1" in
-    --sslkeylogfile)
-      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
-        SSLKEYLOGFILE=$2
-        shift 2
-      else
-        echo "Error: Argument for $1 is missing" >&2
-        exit 1
-      fi
-      ;;
-    --) # end argument parsing
-      shift
-      break
-      ;;
-    -*|--*=) # unsupported flags
-      echo "Error: Unsupported flag $1" >&2
-      exit 1
-      ;;
-    *) # preserve positional arguments
-      PARAMS="$PARAMS $1"
-      shift
-      ;;
+  -mitmp | --mitmproxy-start)
+    info "Starting mitmproxy..."
+    shift
+    if [ -n "$1" ]; then
+      SSLKEYLOGFILE=$1
+    fi
+    info "SSL Key log file path set to $SSLKEYLOGFILE"
+    sudo -u $USER_USERNAME setsid qterminal -e "bash -c 'sudo SSLKEYLOGFILE=$SSLKEYLOGFILE mitmproxy; exec bash'" &
+    exit 0
+    ;;
+
+  -mitmp-insecure | --mitmproxy-start-insecure)
+    info "Starting mitmproxy ssl insecure..."
+    shift
+    if [ -n "$1" ]; then
+      SSLKEYLOGFILE=$1
+    fi
+    info "SSL Key log file path set to $SSLKEYLOGFILE"
+    sudo -u $USER_USERNAME setsid qterminal -e "bash -c 'sudo SSLKEYLOGFILE=$SSLKEYLOGFILE mitmproxy --ssl-insecure; exec bash'" &
+    exit 0
+    ;;
+
+  -ws | --wireshark-start)
+    info "Starting wireshark and setting the tls.keylog_file..."
+    shift
+    if [ -n "$1" ]; then
+      SSLKEYLOGFILE=$1
+    fi
+    info "SSL Key log file path set to $SSLKEYLOGFILE"
+    nohup wireshark -o tls.keylog_file:$SSLKEYLOGFILE &>/dev/null &
+    exit 0
+    ;;
+
+  --) # end argument parsing
+    shift
+    break
+    ;;
+  -* | --*=) # unsupported flags
+    error "Error: Unsupported flag $1" >&2
+    exit 1
+    ;;
+  *) # preserve positional arguments
+    PARAMS="$PARAMS $1"
+    shift
+    ;;
+
   esac
+
 done
-
-
-echo "Wireshark Sniffer Environment Setup Script..."
-echo "Requires: Wifi Adapter, Kali Linux (other debian OSes too), Wireshark, mitmproxy, and SSLKEYLOGFILE environment variable setup."
-
-# Create a new terminal window in Kali Linux and run the following command: sudo SSLKEYLOGFILE=~/Desktop/ssl_key_log.log  mitmproxy --ssl-insecure
-# qterminal -e "bash -c 'sudo SSLKEYLOGFILE=~/Desktop/ssl_key_log.log mitmproxy --ssl-insecure; exec bash'"
-
-# Create a new terminal window in Kali Linux and run the following command: sudo SSLKEYLOGFILE=~/Desktop/ssl_key_log.log  mitmproxy --ssl-insecure
-setsid qterminal -e "bash -c 'sudo SSLKEYLOGFILE=$SSLKEYLOGFILE mitmproxy --ssl-insecure; exec bash'" &
