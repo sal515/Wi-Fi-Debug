f063eb570dbed4d5733f3356c567155129b4e2a2
Salman Rahman <salman.rahman515@gmail.com>
2024-03-15 22:55:19 -0400

    wifi snif - reorganized the function definitions

--- wireshark_network_trace_with_mitmproxy/wifi_debug_function_definitions.sh
+++ wireshark_network_trace_with_mitmproxy/wifi_debug_function_definitions.sh
@@ -1,4 +1,7 @@
-# Logging functions
+# ################################################################################
+# Constants
+# ################################################################################
+
 # Log levels
 LOG_LEVEL_ERROR=0
 LOG_LEVEL_INFO=1
@@ -9,7 +12,10 @@ CURRENT_LOG_LEVEL=$LOG_LEVEL_DEBUG
 
 PRESERVE_GREP_COLOR="--color=always"
 
+# ################################################################################
 # Logging functions
+# ################################################################################
+
 info() {
     if [ $CURRENT_LOG_LEVEL -ge $LOG_LEVEL_INFO ]; then
         local MESSAGE=$1
@@ -34,6 +40,10 @@ error() {
     fi
 }
 
+# ################################################################################
+# Function Definitions
+# ################################################################################
+
 create_symbolic_link() {
     if [ ! -L /usr/local/bin/wifidbg ]; then
         info "Creating symbolic link [wifidbg] for this script..."
@@ -42,15 +52,47 @@ create_symbolic_link() {
     info "[wifidbg] symbol for this script is available system wide"
 }
 
-switch_wlan_interface_mode() {
-    INTERFACE=$1
-    MODE=$2
-    sudo ip link set $INTERFACE down
-    sudo iw dev $INTERFACE set type $MODE
-    sudo ip link set $INTERFACE up
+enable_ssh_service() {
+    sudo systemctl enable ssh
+    sudo systemctl status ssh
 }
 
-# Function for parsing the command line arguments
+find_network() {
+    local interface=$1
+    local ssid_name_pattern=$2
+
+    if [ -z "$ssid_name_pattern" ]; then
+        error "Error: SSID name pattern is required."
+        exit 1
+    fi
+
+    switch_wlan_interface_mode $interface managed
+
+    debug "Scanning for SSID $ssid_name_pattern on interface $interface..."
+
+    local scan_output=$(sudo iw "$interface" scan)
+    # debug "scan_output: $scan_output"
+
+    local bss_blocks=$(echo "$scan_output" |
+        awk -v RS='\nBSS' -v IGNORECASE=1 -v pattern='ssid.{1,3}'"$ssid_name_pattern" '$0 ~ pattern')
+    debug "bss_blocks: $bss_blocks"
+
+    if [ -z "$bss_blocks" ]; then
+        error "Error: SSID $ssid_name_pattern not found."
+        exit 1
+    fi
+
+    SSID_NAME=$(echo "$bss_blocks" |
+        awk -F: 'BEGIN{IGNORECASE=1} /ssid/{print $2}' | tr -d ' ')
+    debug "SSID_NAME: $SSID_NAME"
+
+    SSID_CHANNEL=$(echo "$bss_blocks" |
+        awk 'BEGIN{IGNORECASE=1} /DS Parameter set: channel/{print $5}' | tr -d ' ')
+    debug "SSID_CHANNEL: $SSID_CHANNEL"
+
+    switch_wlan_interface_mode $interface monitor
+}
+
 find_wlan_interface_in_monitor_mode() {
     # Get all wireless interfaces
     INTERFACES=$(iwconfig 2>/dev/null | grep 'wlan' | awk '{print $1}')
@@ -70,29 +112,6 @@ find_wlan_interface_in_monitor_mode() {
         done
         info "No wlan interface in monitor mode."
     fi
-}
-
-start_mitmproxy() {
-    local MITMPROXY_OPTIONS=$1
-    local MITMPROXY_AS_ROOT="" # todo fixme better structure to use sudo
-    # local MITMPROXY_AS_ROOT=$2 # todo fixme better structure to use sudo
-
-    info "Starting mitmproxy with options: $MITMPROXY_OPTIONS"
-    shift
-    if [ -n "$1" ]; then
-        SSLKEYLOGFILE=$1
-    fi
-    info "SSL Key log file path set to $SSLKEYLOGFILE"
-    sudo -u $USER_USERNAME setsid qterminal -e "bash -c '$MITMPROXY_AS_ROOT SSLKEYLOGFILE=$SSLKEYLOGFILE mitmproxy $MITMPROXY_OPTIONS; exec bash'" &
-}
-
-start_wireshark() {
-    info "Starting wireshark and setting the tls.keylog_file..."
-    if [ -n "$1" ]; then
-        SSLKEYLOGFILE=$1
-    fi
-    info "SSL Key log file path set to $SSLKEYLOGFILE"
-    nohup wireshark -o tls.keylog_file:$SSLKEYLOGFILE &>/dev/null &
 }
 
 interface_info() {
@@ -130,38 +149,37 @@ interface_info() {
 
 }
 
-find_network() {
-    local interface=$1
-    local ssid_name_pattern=$2
-
-    if [ -z "$ssid_name_pattern" ]; then
-        error "Error: SSID name pattern is required."
-        exit 1
+start_mitmproxy() {
+    local MITMPROXY_OPTIONS=$1
+    local MITMPROXY_AS_ROOT="" # todo fixme better structure to use sudo
+    # local MITMPROXY_AS_ROOT=$2 # todo fixme better structure to use sudo
+    info "Starting mitmproxy with options: $MITMPROXY_OPTIONS"
+    shift
+    if [ -n "$1" ]; then
+        SSLKEYLOGFILE=$1
     fi
+    info "SSL Key log file path set to $SSLKEYLOGFILE"
+    sudo -u $USER_USERNAME setsid qterminal -e "bash -c '$MITMPROXY_AS_ROOT SSLKEYLOGFILE=$SSLKEYLOGFILE mitmproxy $MITMPROXY_OPTIONS; exec bash'" &
+}
 
-    switch_wlan_interface_mode $interface managed
+start_ssh_service() {
+    sudo systemctl start ssh
+    sudo systemctl status ssh
+}
 
-    debug "Scanning for SSID $ssid_name_pattern on interface $interface..."
-
-    local scan_output=$(sudo iw "$interface" scan)
-    # debug "scan_output: $scan_output"
-
-    local bss_blocks=$(echo "$scan_output" |
-        awk -v RS='\nBSS' -v IGNORECASE=1 -v pattern='ssid.{1,3}'"$ssid_name_pattern" '$0 ~ pattern')
-    debug "bss_blocks: $bss_blocks"
-
-    if [ -z "$bss_blocks" ]; then
-        error "Error: SSID $ssid_name_pattern not found."
-        exit 1
+start_wireshark() {
+    info "Starting wireshark and setting the tls.keylog_file..."
+    if [ -n "$1" ]; then
+        SSLKEYLOGFILE=$1
     fi
+    info "SSL Key log file path set to $SSLKEYLOGFILE"
+    nohup wireshark -o tls.keylog_file:$SSLKEYLOGFILE &>/dev/null &
+}
 
-    SSID_NAME=$(echo "$bss_blocks" |
-        awk -F: 'BEGIN{IGNORECASE=1} /ssid/{print $2}' | tr -d ' ')
-    debug "SSID_NAME: $SSID_NAME"
-
-    SSID_CHANNEL=$(echo "$bss_blocks" |
-        awk 'BEGIN{IGNORECASE=1} /DS Parameter set: channel/{print $5}' | tr -d ' ')
-    debug "SSID_CHANNEL: $SSID_CHANNEL"
-
-    switch_wlan_interface_mode $interface monitor
+switch_wlan_interface_mode() {
+    INTERFACE=$1
+    MODE=$2
+    sudo ip link set $INTERFACE down
+    sudo iw dev $INTERFACE set type $MODE
+    sudo ip link set $INTERFACE up
 }
